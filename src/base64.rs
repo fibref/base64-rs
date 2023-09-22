072aad148920a28f2f3720a2c689a1601a0b14da
Frank <supermouse_233@outlook.com>
2023-09-22 20:36:21 +0800

    easier to read

--- src/base64.rs
+++ src/base64.rs
@@ -1,23 +1,23 @@
 fn u8_to_u6(src: &[u8]) -> Vec<u8> {
     let mut result: Vec<u8> = Vec::new();
     let mut arr = src.iter();
-    let mut part: u8;
+    let mut next_part: u8;
     
     loop {
         match arr.next() {
             Option::Some(&i) => {
                 result.push(i >> 2);
-                part = (i & 0b00000011) << 4;
+                next_part = (i & 0b00000011) << 4;
             },
             Option::None => {break;}
         }
         match arr.next() {
             Option::Some(&i) => {
-                result.push(part | (i >> 4));
-                part = (i & 0b00001111) << 2;
+                result.push(next_part | (i >> 4));
+                next_part = (i & 0b00001111) << 2;
             },
             Option::None => {
-                result.push(part);
+                result.push(next_part);
                 result.push(64);
                 result.push(64); // '='
                 break;
@@ -25,11 +25,11 @@ fn u8_to_u6(src: &[u8]) -> Vec<u8> {
         }
         match arr.next() {
             Option::Some(&i) => {
-                result.push(part | (i >> 6));
+                result.push(next_part | (i >> 6));
                 result.push(i & 0b00111111);
             },
             Option::None => {
-                result.push(part);
+                result.push(next_part);
                 result.push(64); // '='
                 break;
             }
@@ -38,36 +38,37 @@ fn u8_to_u6(src: &[u8]) -> Vec<u8> {
     result
 }
 
+
 //make sure u6.len() % 4 is 0
 fn u6_to_u8(src: Vec<u8>) -> Vec<u8> {
     let mut result: Vec<u8> = Vec::new();
     let mut arr = src.iter();
-    let mut part: u8;
+    let mut next_part: u8;
 
     loop {
         match arr.next() {
-            Option::Some(&i) => {part = i << 2;},
+            Option::Some(&i) => {next_part = i << 2;},
             Option::None => {break;}
         }
         match arr.next() {
             Option::Some(&i) => {
-                result.push(part | (i >> 4));
-                part = i << 4;
+                result.push(next_part | (i >> 4));
+                next_part = i << 4;
             },
             Option::None => {todo!();}
         }
         match arr.next() {
             Option::Some(&i) => {
                 if i == 64 {break;}
-                result.push(part | ((i & 0b00111100) >> 2));
-                part = i << 6;
+                result.push(next_part | ((i & 0b00111100) >> 2));
+                next_part = i << 6;
             },
             Option::None => {todo!();}
         }
         match arr.next() {
             Option::Some(&i) => {
                 if i == 64 {break;}
-                result.push(part | (i & 0b00111111));
+                result.push(next_part | (i & 0b00111111));
             },
             Option::None => {todo!();}
         }
@@ -75,23 +76,25 @@ fn u6_to_u8(src: Vec<u8>) -> Vec<u8> {
     result
 }
 
-fn base64_to_string(src: Vec<u8>) -> String {
-    String::from_utf8(
-        src.iter()
-            .map(|&x| match x {
-                0..=25 => x + 65, // A~Z
-                26..=51 => x + 71, // a~z
-                52..=61 => x - 4, // 0~9
-                62 => 43, // '+'
-                63 => 47, // '/'
-                64 => 61, // '='
-                _ => unreachable!()
-            })
-            .collect()
-    ).unwrap()
+
+fn vec_to_text(src: Vec<u8>) -> String {
+    let chars_utf8: Vec<u8> = src.iter()
+        .map(|&x| match x {
+            0..=25 => x + 65, // A~Z
+            26..=51 => x + 71, // a~z
+            52..=61 => x - 4, // 0~9
+            62 => 43, // '+'
+            63 => 47, // '/'
+            64 => 61, // '='
+            _ => unreachable!()
+        })
+        .collect();
+
+    String::from_utf8(chars_utf8).unwrap()
 }
 
-fn string_to_base64(src: &String) -> Vec<u8> {
+
+fn base64_to_vec(src: &String) -> Vec<u8> {
     src.as_bytes()
         .iter()
         .map(|x| match x {
@@ -106,16 +109,15 @@ fn string_to_base64(src: &String) -> Vec<u8> {
         .collect()
 }
 
+
 pub fn base64_encode(src: &String) -> String {
-    base64_to_string(
+    vec_to_text(
         u8_to_u6(src.as_bytes())
     )
 }
 
+
 pub fn base64_decode(src: &String) -> String {
-    String::from_utf8(
-        u6_to_u8(
-            string_to_base64(src)
-        )
-    ).unwrap()
+    let chars_utf8: Vec<u8> = u6_to_u8(base64_to_vec(src));
+    String::from_utf8(chars_utf8).unwrap()
 }
